cab410ee5cb07b42786cb621e556a1b0493c8fb9
albi23 <piekielny7891>
2019-11-22 13:34:56 +0100

    Connect component with game view

---
+++ src/app/JigsawPuzzleComponent.ts
@@ -0,0 +1,144 @@
+window.onload = function () {
+    loadAll();
+};
+
+window.onbeforeunload = () => {
+    console.log('i \'m there');
+    loadAll();
+};
+
+function loadAll() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const idImg: string = <string>urlParams.get('id');
+    const type: string = <string>urlParams.get('type');
+    const imgName: string = 'image'.concat(idImg).concat('.').concat(type);
+    new JigsawPuzzleComponent(imgName)
+}
+
+enum srcConst {
+    srcImg = "../assets/"
+}
+
+export class JigsawPuzzleComponent {
+
+    private context: CanvasRenderingContext2D;
+    private canvas: HTMLCanvasElement;
+
+    constructor(private imageName: string) {
+        this.canvas = JigsawPuzzleComponent.setDefaultCanvas();
+        this.context = <CanvasRenderingContext2D>this.canvas.getContext("2d");
+        this.getLoadedImg();
+    }
+
+    private getLoadedImg() {
+        this.loadImg().then(() => {
+            console.log('done');
+        }).catch(() => {
+            console.log('not done');
+        })
+    }
+
+    private loadImg(): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+
+            const url = srcConst.srcImg.concat(this.imageName);
+            const newElement: HTMLImageElement = document.createElement('img');
+            newElement.setAttribute("src", url);
+            newElement.setAttribute("alt", this.imageName);
+            console.log(newElement.width);
+            // parent.appendChild(newElement);
+            this.canvas.width = newElement.width;
+            this.canvas.height = newElement.height;
+            this.canvas.classList.add('rounded', 'img-fluid', 'bordered-img');
+            this.context.drawImage(newElement, 0, 0, newElement.width, newElement.height, 0, 0, newElement.width, newElement.height);
+            newElement.onload = () => {
+                resolve(url);
+            };
+            newElement.onerror = () => {
+                reject(url);
+            };
+        });
+    }
+
+    private static setDefaultCanvas(): HTMLCanvasElement {
+        return <HTMLCanvasElement>document.getElementById("can");
+    }
+}
+
+// window.onload = function () {
+//     const stack:HTMLElement[] = [];
+//     const root : HTMLElement = <HTMLElement> document.getElementById('tes');
+//     stack.push(root);
+//     let created = createNewHTMLElement('div',["row"]);
+//     stack.push(created);
+//     /*let*/ created = createNewHTMLElement('div',["col"],"col-1");
+//     stack.push(created);
+//     /*let*/ created  = createNewHTMLElement('img',["rounded", "img-fluid", "flex-img-size", "bordered-img"],"img1","../assets/image1.jpeg");
+//     stack.push(created);
+//     // appendElementsToParent(created2,created3)
+//     // appendElementsToParent(created,created2)
+//     // appendElementsToParent(root,created)
+//     while (stack.length > 1){
+//         let child = <HTMLElement>stack.pop();
+//         let parent = <HTMLElement>stack.pop();
+//         stack.push(appendElementsToParent(parent,child))
+//     }
+// };
+//
+// function loadIMG(url: string, id: string): Promise<any> {
+//
+//     return new Promise<any>((resolve, reject) => {
+//
+//         const parent: HTMLDivElement = <HTMLDivElement>document.getElementById(id);
+//         let newElement: HTMLImageElement = document.createElement('img');
+//         newElement.setAttribute("src", url);
+//         newElement.setAttribute("alt", url);
+//         newElement.setAttribute("width", "30%");
+//         newElement.setAttribute("height", "30%");
+//         parent.appendChild(newElement);
+//         newElement.onload = function () {
+//             resolve(url);
+//         };
+//         newElement.onerror = function () {
+//             reject(url);
+//         };
+//     });
+// }
+//
+//
+// function appendElementsToParent(parent: HTMLElement, child: HTMLElement):HTMLElement {
+//     parent.appendChild(child);
+//     return parent;
+// }
+//
+// function createNewHTMLElement(newElementType: string, classNames?: string[], id?: string, url?:string): HTMLElement {
+//     let newElement: HTMLElement = <HTMLElement>document.createElement(newElementType);
+//     if (classNames) newElement = <HTMLElement>addClasses(newElement, classNames);
+//     if (id) newElement = <HTMLElement>addId(newElement, id);
+//     if (id) newElement = <HTMLElement>addSrc(newElement, url);
+//     return newElement;
+// }
+//
+// function addClasses(element: HTMLElement, classNames?: string[]): HTMLElement {
+//     if (!element) return element;
+//     if (classNames && classNames.length > 0) {
+//         for (const newClass of classNames) {
+//             element.classList.add(newClass)
+//         }
+//     }
+//     return element;
+// }
+//
+// function addId(element: HTMLElement,id: string): HTMLElement {
+//     if (!element) return element;
+//     element.setAttribute("id", id);
+//     return element;
+// }
+//
+// function addSrc(element: HTMLElement,url?: string): HTMLElement {
+//     if (!element) return element;
+//     if (typeof url === "string") {
+//         element.setAttribute("src", url);
+//     }
+//     return element;
+// }
