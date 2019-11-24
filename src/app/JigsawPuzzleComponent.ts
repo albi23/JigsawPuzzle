3ac7e070c2137ea04e28596e9b65b192c71fb6b9
albi23 <piekielny7891>
2019-11-25 00:46:33 +0100

    Now is work nice

--- src/app/JigsawPuzzleComponent.ts
+++ src/app/JigsawPuzzleComponent.ts
@@ -17,7 +17,8 @@ function loadAll() {
 
 enum srcConst {
     srcImg = "../assets/",
-    PUZZLE_HOVER_TINT = "#009900"
+    PUZZLE_HOVER_TINT = "#009900",
+    PUZZLE_MAIN = "#ff0012"
 }
 
 export class JigsawPuzzleComponent {
@@ -28,6 +29,7 @@ export class JigsawPuzzleComponent {
     mouse: Point2D = new Point2D(0, 0);
     currentPiece: Piece | null = new Piece(0, 0, 0, 0);
     currentDropPiece: Piece | null = null;
+    mainPiece: Piece = new Piece(0,0,0,0);
     PUZZLE_COL: number = 4;
     PUZZLE_ROW: number = 4;
     pieceWidth: number = 0;
@@ -49,7 +51,9 @@ export class JigsawPuzzleComponent {
             (<HTMLInputElement>(document.getElementById('btn')))
                 .addEventListener('click',()=>{this.changeColAndRow();});
         }).catch(() => {
-            console.log('not done');
+            const alertElem = <HTMLDivElement>(document.getElementById('alert'));
+            alertElem.style.display ='block';
+            alertElem.innerText ='Some error occurred during load image';
         })
     }
 
@@ -113,6 +117,10 @@ export class JigsawPuzzleComponent {
             piece.setYPos(yPos);
             this.drawScaledImage(piece.getX(), piece.getY(), this.pieceWidth, this.pieceHeight,
                 xPos, yPos, this.pieceWidth, this.pieceHeight);
+            if (piece.getX() === 0 && piece.getY() === 0 ){
+                this.drawMainPiece(piece);
+                this.mainPiece = piece;
+            }
             this.context.strokeRect(xPos, yPos, this.pieceWidth, this.pieceHeight);
             xPos += this.pieceWidth;
             if (xPos >= this.getPuzzleWidth()) {
@@ -181,9 +189,8 @@ export class JigsawPuzzleComponent {
     }
 
     private onPuzzleClick(mouseEvent: MouseEvent) {
-        console.log('x: ', mouseEvent.clientX, '  y: ', mouseEvent.clientY);
         const clientRects : ClientRect = this.canvas.getClientRects()[0];
-        this.setMousePostion(mouseEvent,clientRects);
+        this.setMousePosition(mouseEvent,clientRects);
         this.currentPiece = this.checkPieceClicked();
         if (this.currentPiece != null) {
             this.context.clearRect(this.currentPiece.getXPos(), this.currentPiece.getYPos(),
@@ -193,7 +200,6 @@ export class JigsawPuzzleComponent {
             this.drawScaledImage( this.currentPiece.getX(), this.currentPiece.getY(), this.pieceWidth, this.pieceHeight,
                 this.mouse.getX() - (this.pieceWidth / 2), this.mouse.getY() - (this.pieceHeight / 2), this.pieceWidth, this.pieceHeight)
             this.context.restore();
-
             document.onmousemove = (e: MouseEvent) => {
                 this.updatePuzzle(e,clientRects)
             };
@@ -213,7 +219,11 @@ export class JigsawPuzzleComponent {
                 this.mouse.getY() > (piece.getYPos() + this.pieceHeight)) {
 //PIECE NOT HIT
             } else {
-                return piece;
+                if ((piece !== this.currentPiece) &&
+                    ((piece.getXPos() == this.mainPiece.getXPos() && Math.abs(piece.getYPos()-this.mainPiece.getYPos()) === this.pieceHeight) ||
+                    (piece.getYPos() == this.mainPiece.getYPos() && Math.abs(piece.getXPos()-this.mainPiece.getXPos()) === this.pieceWidth))){
+                    return piece;
+                }
             }
         }
         return null;
@@ -221,7 +231,7 @@ export class JigsawPuzzleComponent {
 
     private updatePuzzle(e: MouseEvent, clientRects : ClientRect) {
         this.currentDropPiece = null;
-        this.setMousePostion(e,clientRects);
+        this.setMousePosition(e,clientRects);
         this.context.clearRect(0, 0, this.getPuzzleWidth(), this.getPuzzleHeight());
         let piece: Piece;
         for (let i = 0; i < this.pieces.length; i++) {
@@ -231,27 +241,30 @@ export class JigsawPuzzleComponent {
             }
             this.drawScaledImage(piece.getX(), piece.getY(), this.pieceWidth, this.pieceHeight, piece.getXPos(), piece.getYPos(),
                 this.pieceWidth, this.pieceHeight);
-            this.context.strokeRect(piece.getXPos(), piece.getYPos(), this.pieceWidth,
-                this.pieceHeight);
+
+            this.context.strokeRect(piece.getXPos(), piece.getYPos(), this.pieceWidth, this.pieceHeight);
             if (this.currentDropPiece == null) {
                 if (this.mouse.getX() < piece.getXPos() || this.mouse.getX() >
                     (piece.getXPos() + this.pieceWidth) || this.mouse.getY() < piece.getYPos()
                     || this.mouse.getY() > (piece.getYPos() + this.pieceHeight)) {
 //NOT OVER
                 } else {
+                    if (piece === this.mainPiece){
+                        this.currentDropPiece = piece;
+                        this.context.save();
+                        this.context.globalAlpha = .4;
+                        this.context.fillStyle = srcConst.PUZZLE_HOVER_TINT;
+                        this.context.fillRect(this.currentDropPiece.getXPos(), this.currentDropPiece.getYPos(),
+                            this.pieceWidth, this.pieceHeight);
+                        this.context.restore();
+                    }
                     this.currentDropPiece = piece;
-                    this.context.save();
-                    this.context.globalAlpha = .4;
-                    this.context.fillStyle = srcConst.PUZZLE_HOVER_TINT;
-                    this.context.fillRect(this.currentDropPiece.getXPos(),
-                        this.currentDropPiece.getYPos(), this.pieceWidth,
-                        this.pieceHeight);
-                    this.context.restore();
+
                 }
             }
         }
         this.context.save();
-        this.context.globalAlpha = .6;;
+        this.context.globalAlpha = .6;
         if (this.currentPiece){
             this.drawScaledImage(this.currentPiece.getX(), (this.currentPiece).getY(), this.pieceWidth, this.pieceHeight,
                 this.mouse.getX() - (this.pieceWidth / 2), this.mouse.getY() - (this.pieceHeight / 2), this.pieceWidth, this.pieceHeight)
@@ -260,7 +273,7 @@ export class JigsawPuzzleComponent {
         this.context.strokeRect(this.mouse.getX() - (this.pieceWidth / 2), this.mouse.getY() - (this.pieceHeight / 2), this.pieceWidth, this.pieceHeight);
     }
 
-    private setMousePostion(e: MouseEvent, clientRects : ClientRect){
+    private setMousePosition(e: MouseEvent, clientRects : ClientRect){
         if (e.clientX || e.clientX == 0) {
             this.mouse.setX(e.clientX - clientRects.left);
             this.mouse.setY(e.clientY - clientRects.top);
@@ -284,6 +297,13 @@ export class JigsawPuzzleComponent {
         this.resetPuzzleAndCheckWin();
     }
 
+    private drawMainPiece(piece:Piece){
+        this.context.globalAlpha = 1;
+        this.context.fillStyle = srcConst.PUZZLE_MAIN;
+        this.context.fillRect(piece.getXPos(), piece.getYPos(), this.pieceWidth, this.pieceHeight);
+        this.context.restore();
+    }
+
     private resetPuzzleAndCheckWin() {
         this.context.clearRect(0, 0, this.getPuzzleWidth(), this.getPuzzleHeight());
         let gameWin = true;
@@ -291,19 +311,28 @@ export class JigsawPuzzleComponent {
         let piece: Piece;
         for (let i = 0; i < this.pieces.length; i++) {
             piece = this.pieces[i];
-            this.context.drawImage(this.img, piece.getX()/this.xScale, piece.getY()/this.yScale,
-                this.pieceWidth/this.xScale, this.pieceHeight/this.yScale, piece.getXPos(),
+            this.drawScaledImage( piece.getX(), piece.getY(), this.pieceWidth, this.pieceHeight, piece.getXPos(),
                 piece.getYPos(), this.pieceWidth, this.pieceHeight);
             this.context.strokeRect(piece.getXPos(), piece.getYPos(),
                 this.pieceWidth, this.pieceHeight);
+            if (piece === this.mainPiece) {
+                this.drawMainPiece(piece);
+            }
             if (piece.getXPos() != piece.getX() || piece.getYPos() != piece.getY()) {
                 gameWin = false;
             }
         }
         if (gameWin) {
-            console.log('Wygrałeś chujku');
-            // setTimeout(gameOver,500);
+            console.log('You WIN!');
+            setTimeout(this.gameOver,500);
         }
+    }
+
+    private  gameOver(){
+        document.onmousedown = null;
+        document.onmousemove = null;
+        document.onmouseup = null;
+        loadAll();
     }
 }
 
